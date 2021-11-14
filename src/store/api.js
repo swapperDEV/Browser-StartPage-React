5ff2ad8f1428a5c9a0e08d27cbe139079a3f2f1e
Wiktor Maciążek <75098062+swapperDEV@users.noreply.github.com>
2021-11-14 20:13:15 +0100

    Delete src directory

--- src/store/api.js
+++
@@ -1,3 +0,0 @@
-export const WEATHER_API_LINK = 'https://api.openweathermap.org/data/2.5/weather?q='
-export const WEATHER_API_KEY = '&appid=4635fbddb66396f2257be8496e9c64db'
-export const WEATHER_API_UNITS = '&units=metric'
